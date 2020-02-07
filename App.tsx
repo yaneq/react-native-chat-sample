40754051b9eed0e90fb7249db78d2a2e2fe9c838
Jan Koeppen <jan@yaneq.com>
2020-02-06 22:32:36 -0400

    load fonts async

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState} from 'react';
 import NavigationContainer from './navigation/navigation';
 import {store} from './state/store';
 import {Provider} from 'react-redux';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
 
-const AppContainer = () => (
-  <Provider store={store}>
-    <NavigationContainer />
-  </Provider>
-);
+const _cacheResourcesAsync = async () => {
+  const cacheFonts = await Font.loadAsync({
+    'quicksand': require('./assets/fonts/Quicksand-Regular.otf'),
+    'quicksand-bold': require('./assets/fonts/Quicksand-Bold.otf'),
+  });
+
+  return Promise.all([cacheFonts]);
+}
+const AppContainer = () => {
+  const [ready, setReady] = useState(false);
+  if (!ready) {
+    console.log('in render')
+    return (
+      <AppLoading
+        startAsync={_cacheResourcesAsync}
+        onFinish={() => {console.log('IM READY'); setReady(true)}}
+        onError={console.warn}
+      />
+    );
+  }
+
+  console.log('should only be called if ready')
+  return (
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
+  )
+};
 
 export default AppContainer;
